fa596e2ad3b54f58f3a0f3b11b34c85c5745905f
Michael Clausen <michael.clausen@hevs.ch>
2019-04-30 08:46:31 +0200

    Initial RabbitMQ 3.7 image for cloud.iO

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM rabbitmq:3.7-alpine
+
+ADD rabbitmq_auth_backend_amqp-20171201-3.7.x.ez /opt/rabbitmq/plugins/
+RUN rabbitmq-plugins enable --offline rabbitmq_auth_backend_amqp rabbitmq_auth_backend_cache rabbitmq_auth_mechanism_ssl rabbitmq_management rabbitmq_mqtt
+
+ADD rabbitmq-env.conf /etc/rabbitmq/
+ADD rabbitmq.conf /etc/rabbitmq/
+ADD entrypoint.sh /
+RUN chmod +x /entrypoint.sh
+
+ENV CA_CERT ""
+ENV SERVER_CERT ""
+ENV SERVER_KEY ""
+
+EXPOSE 5671 8883 15672
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Save certificates and keys passed in the environment.
+if [[ "$CA_CERT" == "-----BEGIN CERTIFICATE-----"* ]]; then
+	echo "$CA_CERT" > /etc/rabbitmq/ca.crt
+else
+	echo "Missing CA Certificate (CA_CERT) environment variable or invalid certificate was set!"
+	exit 1
+fi
+
+if [[ "$SERVER_CERT" == "-----BEGIN CERTIFICATE-----"* ]]; then
+	echo "$SERVER_CERT" > /etc/rabbitmq/server.crt
+else
+	echo "Missing Server Certificate (SERVER_CERT) environment variable or invalid certificate was set!"
+	exit 1
+fi
+
+if [[ "$SERVER_KEY" == "-----BEGIN PRIVATE KEY-----"* ]]; then
+	echo "$SERVER_KEY" > /etc/rabbitmq/server.key
+else
+	echo "Missing Server Key (SERVER_KEY) environment variable or invalid key was set!"
+	exit 1
+fi
+
+# Configure RabbitMQ Broker after boot.
+configure_rabbitmq_server() {
+	rabbitmqctl wait /var/run/rabbitmq.pid
+
+	echo " "
+	echo "========== Post-Startup RabbitMQ Broker Configuration =========="
+
+	# Add admin user.
+	if [ -n "${ADMIN_PASSWORD}" ]
+	then
+	    echo "Admin user for this broker is 'admin' with password '${ADMIN_PASSWORD}'"
+		rabbitmqctl add_user admin ${ADMIN_PASSWORD}
+		rabbitmqctl set_user_tags admin administrator
+		rabbitmqctl set_permissions admin ".*" ".*" ".*"
+	else
+		echo "No admin user is created."
+	fi
+
+	# Add cloud.iO micro-services user.
+	rabbitmqctl add_user cloudio_services DUMMY
+	rabbitmqctl clear_password cloudio_services
+	rabbitmqctl set_permissions cloudio_services ".*" ".*" ".*"
+
+	echo " "
+	echo "========== Finished, cloud.iO Broker ready to use. =========="
+}
+configure_rabbitmq_server &
+
+rabbitmq-server
